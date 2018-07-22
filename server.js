c3de733b229f24af02d4070ad5d18770e7762cbd
tomas.havelka <tomas.havelka@economia.cz>
2018-07-22 17:49:03 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+const app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+  const now = new Date().toString()
+  const log = `${now}: ${req.method} ${req.url}`
+  
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      cconsole.log('Unable to append to server.log')
+    }
+  })
+  next()
+})
+
+app.use((req, res, next) => {
+  res.render('maintance.hbs')
+})
+
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear())
+hbs.registerHelper('screamIt', (text) => text.toUpperCase())
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Homepage',
+    welcomeMessage: "You've done it, bitch!"
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  })
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'You fucked something up.'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000')
+})
